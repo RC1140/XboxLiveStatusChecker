a61e71b653fe1ff0c64ecd8277f96f650cb355f5
stalkerh <jameelhaffejee@gmail.com>
2009-10-09 20:05:13 +0200

    Initial Checkin

---
+++ Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace TestApp
+{
+    partial class LiveStatuses
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.dgUsers = new System.Windows.Forms.DataGridView();
+            this.btnLoadUsers = new System.Windows.Forms.Button();
+            this.openFileDialogUsers = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblNumUsersOnline = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgUsers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Refresh";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(130, 16);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 13);
+            this.lblStatus.TabIndex = 4;
+            // 
+            // dgUsers
+            // 
+            this.dgUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgUsers.Location = new System.Drawing.Point(12, 65);
+            this.dgUsers.Name = "dgUsers";
+            this.dgUsers.Size = new System.Drawing.Size(448, 329);
+            this.dgUsers.TabIndex = 5;
+            // 
+            // btnLoadUsers
+            // 
+            this.btnLoadUsers.Location = new System.Drawing.Point(342, 12);
+            this.btnLoadUsers.Name = "btnLoadUsers";
+            this.btnLoadUsers.Size = new System.Drawing.Size(118, 23);
+            this.btnLoadUsers.TabIndex = 6;
+            this.btnLoadUsers.Text = "Load Users From File";
+            this.btnLoadUsers.UseVisualStyleBackColor = true;
+            this.btnLoadUsers.Click += new System.EventHandler(this.btnLoadUsers_Click);
+            // 
+            // openFileDialogUsers
+            // 
+            this.openFileDialogUsers.FileName = "users.txt";
+            this.openFileDialogUsers.Title = "Users List";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Number Of Users Online";
+            // 
+            // lblNumUsersOnline
+            // 
+            this.lblNumUsersOnline.AutoSize = true;
+            this.lblNumUsersOnline.Location = new System.Drawing.Point(141, 42);
+            this.lblNumUsersOnline.Name = "lblNumUsersOnline";
+            this.lblNumUsersOnline.Size = new System.Drawing.Size(0, 13);
+            this.lblNumUsersOnline.TabIndex = 8;
+            // 
+            // LiveStatuses
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 406);
+            this.Controls.Add(this.lblNumUsersOnline);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLoadUsers);
+            this.Controls.Add(this.dgUsers);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.button1);
+            this.Name = "LiveStatuses";
+            this.Text = "Xbox Live Statuses";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgUsers)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.DataGridView dgUsers;
+        private System.Windows.Forms.Button btnLoadUsers;
+        private System.Windows.Forms.OpenFileDialog openFileDialogUsers;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblNumUsersOnline;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TestApp
+{
+    public partial class LiveStatuses : Form
+    {
+        private DataTable liveUsers;
+        List<string> usersToCheck = new List<string>();
+        private BackgroundWorker[] workingGamers;
+
+        public LiveStatuses()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            #region Setup Datatable
+            liveUsers = new DataTable("liveUsers");
+            liveUsers.Columns.Add("gamercard");
+            liveUsers.Columns.Add("online");
+            liveUsers.Columns.Add("last_seen_online");
+
+            liveUsers.PrimaryKey = new DataColumn[] { liveUsers.Columns["gamercard"] };
+            #endregion
+        }
+
+        private void GetUsersStatus(string Gamercard)
+        {
+            try
+            {
+                DataRow userInfo = liveUsers.Rows.Find(Gamercard);
+                bool newData = (userInfo == null);
+                if (newData)
+                {
+                    userInfo = liveUsers.NewRow();
+                }
+
+                HttpWebRequest sd =
+                    (HttpWebRequest)HttpWebRequest.Create("http://xboxapi.duncanmackenzie.net/gamertag.ashx?GamerTag=" + Gamercard);
+
+                StreamReader sddd = new StreamReader(sd.GetResponse().GetResponseStream());
+
+                string xml = sddd.ReadToEnd();
+                int startpoint = xml.IndexOf("Online");
+                string status = xml.Substring(startpoint + 7, xml.IndexOf("Online", startpoint + 1) - startpoint);
+                int startpoint2 = xml.IndexOf("LastSeen");
+                string lastSeenOnline = xml.Substring(startpoint2 + 9, xml.IndexOf("LastSeen", startpoint2 + 1) - startpoint2);
+
+                userInfo["gamercard"] = Gamercard;
+                userInfo["online"] = status.Substring(0, status.Length - 9);
+                userInfo["last_seen_online"] = lastSeenOnline.Substring(0, lastSeenOnline.Length - 11);
+
+                if (newData)
+                {
+                    liveUsers.Rows.Add(userInfo);
+                }
+            }
+            catch 
+            {
+
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int totalNumberOfUsers = usersToCheck.Count;
+            int groupingIndex = 0;
+            decimal numberOfWorkersRequired = decimal.Round(decimal.Divide(totalNumberOfUsers, 20), 0,MidpointRounding.AwayFromZero);
+            if (numberOfWorkersRequired == 0)
+            {
+                numberOfWorkersRequired = 1;
+            }
+            lblStatus.Text = "Updating";
+
+            workingGamers = new BackgroundWorker[Convert.ToInt32(numberOfWorkersRequired)];
+            for (int i = 0; i < workingGamers.Length; i++)
+            {
+                workingGamers[i] = new BackgroundWorker();
+                workingGamers[i].DoWork += new DoWorkEventHandler(backgroundWorker1_DoWork);
+                workingGamers[i].RunWorkerCompleted += new RunWorkerCompletedEventHandler(backgroundWorker1_RunWorkerCompleted);
+            }
+            
+            for (int i = 0; i < workingGamers.Length; i++)
+            {
+                totalNumberOfUsers -= 20;
+                if (totalNumberOfUsers >= 20)
+                {
+                    workingGamers[i].RunWorkerAsync(usersToCheck.GetRange(groupingIndex, 20));
+                }
+                else
+                {
+                    workingGamers[i].RunWorkerAsync(usersToCheck.GetRange(groupingIndex, (20 + totalNumberOfUsers)));
+                    break;
+                }
+                groupingIndex += 20;
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            UpdateUsersStatus(e);
+        }
+
+        private void UpdateUsersStatus(DoWorkEventArgs e)
+        {
+            List<string> pUsersToCheck = (List<string>)e.Argument;
+            foreach (var gamertag in pUsersToCheck)
+            {
+                GetUsersStatus(gamertag);
+            }
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            lblNumUsersOnline.Text = liveUsers.Select("online = 'true'").Length.ToString();
+            dgUsers.DataSource = liveUsers;
+            dgUsers.Refresh();
+            lblStatus.Text = "Update Complete";
+        }
+
+        private void btnLoadUsers_Click(object sender, EventArgs e)
+        {
+            if (openFileDialogUsers.ShowDialog() == DialogResult.OK)
+            {
+                usersToCheck.Clear();
+
+                StreamReader sr = new StreamReader(openFileDialogUsers.FileName);
+
+                while (!sr.EndOfStream)
+                {
+                    usersToCheck.Add(sr.ReadLine());
+                }
+                lblStatus.Text = "Users Loaded Successfully";
+            }
+        }
+    }
+}
